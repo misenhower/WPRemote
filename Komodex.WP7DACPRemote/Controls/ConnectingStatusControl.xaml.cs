1199a0e2e668d3d0dd49cb5a01962957703b1f63
Matt Isenhower <matt@isenhower.com>
2010-11-14 21:36:20 -0800

    Added new ConnectingStatusControl

---
+++ Komodex.WP7DACPRemote/Controls/ConnectingStatusControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Komodex.WP7DACPRemote.Controls
+{
+    public partial class ConnectingStatusControl : UserControl
+    {
+        public ConnectingStatusControl()
+        {
+            InitializeComponent();
+
+            LayoutRoot.DataContext = this;
+        }
+
+        public static readonly DependencyProperty ShowProgressProperty =
+            DependencyProperty.Register("ShowProgress", typeof(bool), typeof(ConnectingStatusControl),
+            new PropertyMetadata((bool)false));
+
+        public bool ShowProgress
+        {
+            get { return (bool)GetValue(ShowProgressProperty); }
+            set { SetValue(ShowProgressProperty, value); }
+        }
+
+        public static readonly DependencyProperty LibraryNameProperty =
+            DependencyProperty.Register("LibraryName", typeof(string), typeof(ConnectingStatusControl),
+            new PropertyMetadata((string)string.Empty));
+
+        public string LibraryName
+        {
+            get { return (string)GetValue(LibraryNameProperty); }
+            set { SetValue(LibraryNameProperty, value); }
+        }
+
+    }
+}
