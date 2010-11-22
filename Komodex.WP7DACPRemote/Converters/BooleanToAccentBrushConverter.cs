a978d60f0cc0f01457c1525439c640c39e2a2de6
Matt Isenhower <matt@isenhower.com>
2010-11-21 21:48:43 -0800

    Implemented new grouped listbox for the artists list

---
+++ Komodex.WP7DACPRemote/Converters/BooleanToAccentBrushConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using Komodex.DACP;
+
+namespace Komodex.WP7DACPRemote.Converters
+{
+    public class BooleanToAccentBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var flag = false;
+            if (value is bool)
+            {
+                flag = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                var nullable = (bool?)value;
+                flag = nullable.GetValueOrDefault();
+            }
+            if (parameter != null)
+            {
+                if (bool.Parse((string)parameter))
+                {
+                    flag = !flag;
+                }
+            }
+            if (flag)
+            {
+                return (SolidColorBrush)Application.Current.Resources["PhoneAccentBrush"];
+            }
+            else
+            {
+                return (SolidColorBrush)Application.Current.Resources["PhoneChromeBrush"];
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
