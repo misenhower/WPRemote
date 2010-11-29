da11f361351e2d52f488f84f83f0356820515a55
Matt Isenhower <matt@isenhower.com>
2010-11-29 14:45:02 -0800

    Added some more functionality to MainPage

--- Komodex.WP7DACPRemote/MainPage.xaml.cs
+++ Komodex.WP7DACPRemote/MainPage.xaml.cs
@@ -23,7 +23,47 @@ namespace Komodex.WP7DACPRemote
         public MainPage()
         {
             InitializeComponent();
+
+            // ApplicationBar buttons and menu items must be referenced at run time
+            btnPlayPause = (ApplicationBarIconButton)ApplicationBar.Buttons[1];
         }
+
+        private ApplicationBarIconButton btnPlayPause = null;
+        private readonly Uri iconPlay = new Uri("/icons/appbar.transport.play.rest.png", UriKind.Relative);
+        private readonly Uri iconPause = new Uri("/icons/appbar.transport.pause.rest.png", UriKind.Relative);
+
+        #region Overrides
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            UpdatePlayPauseButton();
+        }
+
+        protected override void DACPServer_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.DACPServer_PropertyChanged(sender, e);
+
+            if (e.PropertyName == "PlayState")
+                UpdatePlayPauseButton();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void UpdatePlayPauseButton()
+        {
+            if (DACPServer.PlayState == PlayStates.Playing)
+                btnPlayPause.IconUri = iconPause;
+            else
+                btnPlayPause.IconUri = iconPlay;
+        }
+
+        #endregion
+
+        #region Actions
 
         private void btnNowPlaying_Click(object sender, RoutedEventArgs e)
         {
@@ -34,5 +74,38 @@ namespace Komodex.WP7DACPRemote
         {
             NavigationManager.OpenMainLibraryPage();
         }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationManager.OpenLibraryChooserPage();
+        }
+
+        private void mnuSettings_Click(object sender, EventArgs e)
+        {
+            NavigationManager.OpenLibraryChooserPage();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            DACPServer.SendPrevItemCommand();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            DACPServer.SendNextItemCommand();
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            DACPServer.SendPlayPauseCommand();
+        }
+
+        #endregion
+
     }
 }
