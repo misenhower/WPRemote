5537745dcc3d98eebf5c4701818a3afba4ae3e16
Matt Isenhower <matt@isenhower.com>
2010-12-02 16:15:46 -0800

    Added basic overlay for the repeat/shuffle controls

--- Komodex.WP7DACPRemote/NowPlayingPage.xaml.cs
+++ Komodex.WP7DACPRemote/NowPlayingPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Komodex.DACP;
+using System.Windows.Threading;
 
 namespace Komodex.WP7DACPRemote
 {
@@ -24,13 +25,43 @@ namespace Komodex.WP7DACPRemote
             InitializeStandardTransportApplicationBar();
 
             AnimationContext = LayoutRoot;
+
+            playControlDisplayTimer.Interval = TimeSpan.FromSeconds(5);
+            playControlDisplayTimer.Tick += new EventHandler(playControlDisplayTimer_Tick);
         }
 
         #region Overrides
 
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            HidePlayControls(false);
+        }
+
         #endregion
 
-        #region Methods
+        #region Play Control Management
+
+        protected DispatcherTimer playControlDisplayTimer = new DispatcherTimer();
+
+        void playControlDisplayTimer_Tick(object sender, EventArgs e)
+        {
+            playControlDisplayTimer.Stop();
+            HidePlayControls();
+        }
+
+        private void ShowPlayControls(bool useTransitions = true)
+        {
+            playControlDisplayTimer.Stop();
+            VisualStateManager.GoToState(this, "PlayControlsVisibleState", useTransitions);
+            playControlDisplayTimer.Start();
+        }
+
+        private void HidePlayControls(bool useTransitions = true)
+        {
+            playControlDisplayTimer.Stop();
+            VisualStateManager.GoToState(this, "PlayControlsHiddenState", useTransitions);
+        }
 
         #endregion
 
@@ -44,6 +75,17 @@ namespace Komodex.WP7DACPRemote
         private void btnArtist_Click(object sender, RoutedEventArgs e)
         {
             NavigationManager.OpenArtistPage(DACPServer.CurrentArtist);
+        }
+
+        private void bdrPlayControls_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPlayControls();
+            e.Handled = true;
+        }
+
+        private void Page_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            HidePlayControls();
         }
 
         #endregion
