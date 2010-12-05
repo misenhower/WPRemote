708e896bc672ab03bfe5fae066d50f565f96d338
Matt Isenhower <matt@isenhower.com>
2010-12-04 20:19:24 -0800

    Updating some of the information in AssemblyInfo.cs

--- Komodex.WP7DACPRemote/Properties/AssemblyInfo.cs
+++ Komodex.WP7DACPRemote/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Komodex.WP7DACPRemote")]
+[assembly: AssemblyTitle("Remote")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Komodex.WP7DACPRemote")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("Komodex Systems LLC")]
+[assembly: AssemblyProduct("Komodex Remote")]
+[assembly: AssemblyCopyright("Copyright 2010 Komodex Systems LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
