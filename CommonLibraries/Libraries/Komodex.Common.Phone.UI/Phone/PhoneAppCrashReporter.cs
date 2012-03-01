b3380f88d767a2d1bf75139f5072b20b0ff8d6b2
Matt Isenhower <matt@isenhower.com>
2012-03-01 15:56:17 -0800

    Added CrashReporter and PhoneAppCrashReporter classes, adapted from Server Pulse

---
+++ CommonLibraries/Libraries/Komodex.Common.Phone.UI/Phone/PhoneAppCrashReporter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Navigation;
+using System.IO;
+using Microsoft.Phone.Shell;
+
+namespace Komodex.Common.Phone
+{
+    public static class PhoneAppCrashReporter
+    {
+        private static PhoneApplicationFrame RootFrame = null;
+        private static bool IsObscured = false;
+        private static bool IsLocked = false;
+
+
+        public static void Initialize(Application application, PhoneApplicationFrame rootFrame)
+        {
+            // Initialize the CrashReporter if it hasn't already been initialized
+            CrashReporter.Initialize(application);
+
+            // Get the RootFrame object
+            RootFrame = rootFrame;
+
+            // Hook into navigation failed event
+            RootFrame.NavigationFailed += new NavigationFailedEventHandler(RootFrame_NavigationFailed);
+
+            // Hook into obscured/unobscured events
+            RootFrame.Obscured += new EventHandler<ObscuredEventArgs>(RootFrame_Obscured);
+            RootFrame.Unobscured += new EventHandler(RootFrame_Unobscured);
+
+            // Add the navigation info callback to the CrashReporter
+            CrashReporter.AdditionalLogInfoCallbacks.Add(NavigationInfoCallback);
+        }
+
+
+        #region Events
+
+        private static void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            CrashReporter.LogException(e.Exception, "Navigation Failed");
+        }
+
+        private static void RootFrame_Obscured(object sender, ObscuredEventArgs e)
+        {
+            IsObscured = true;
+            IsLocked = e.IsLocked;
+        }
+
+        private static void RootFrame_Unobscured(object sender, EventArgs e)
+        {
+            IsObscured = false;
+            IsLocked = false;
+        }
+
+        #endregion
+
+        #region Log Info Callback
+
+        private static void NavigationInfoCallback(TextWriter writer)
+        {
+            try
+            {
+                writer.WriteLine("-> Current page: " + RootFrame.CurrentSource);
+            }
+            catch { } // If we're not in the UI thread, attempting to access RootFrame.CurrentSource will throw an UnauthorizedAccessException
+
+            writer.WriteLine("-> Startup Mode: " + PhoneApplicationService.Current.StartupMode);
+            writer.WriteLine("-> Obscured: " + ((IsObscured) ? "Yes" : "No"));
+            writer.WriteLine("-> Locked: " + ((IsLocked) ? "Yes" : "No"));
+        }
+
+        #endregion
+
+    }
+}
---
+++ CommonLibraries/Libraries/Komodex.Common/Phone/CrashReporter.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Navigation;
+using System.IO.IsolatedStorage;
+using System.IO;
+using Microsoft.Phone.Shell;
+using System.Text;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Komodex.Common.Phone
+{
+    public static class CrashReporter
+    {
+        // Error Report URL parameters
+        private const string ErrorReportURL = "http://sys.komodex.com/wp7/crashreporter/";
+
+        // The filename for crash reports in isolated storage 
+        private const string ErrorLogFilename = "ApplicationErrorLog.log";
+
+        private static readonly string LargeDashes = new string('=', 80);
+        private static readonly string SmallDashes = new string('-', 80);
+
+        private static bool _initialized = false;
+        /// <summary>
+        /// Initializes the crash reporter. Unless this is a background agent, use
+        /// PhoneAppCrashReporter.Initialize(application, rootFrame) instead.
+        /// </summary>
+        /// <param name="application"></param>
+        public static void Initialize(Application application)
+        {
+            if (_initialized)
+                return;
+
+            // Hook into application unhandled exceptions
+            application.UnhandledException += App_UnhandledException;
+
+            // Send previous log if it exists
+            SendExceptionLog();
+
+            _initialized = true;
+        }
+
+        #region Events
+
+        private static void App_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            LogException(e.ExceptionObject, "Unhandled Exception");
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static void LogException(Exception e, string type = null, bool sendImmediately = false)
+        {
+            // Exception Info
+            string exceptionInfo = "Exception Information:\r\n";
+            exceptionInfo += e.ToString();
+
+            LogMessage(exceptionInfo, type, sendImmediately);
+        }
+
+        public static void LogMessage(string message, string type = null, bool sendImmediately = false)
+        {
+            try
+            {
+                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (TextWriter writer = new StreamWriter(store.OpenFile(ErrorLogFilename, FileMode.Append)))
+                    {
+                        writer.WriteLine(LargeDashes);
+
+                        // Error type
+                        writer.WriteLine(type ?? "Application Error");
+
+                        // Application name
+                        writer.WriteLine("-> Product: " + Utility.ApplicationName);
+
+                        // Application version
+                        writer.Write("-> Version: " + Utility.ApplicationVersion);
+#if DEBUG
+                        writer.Write(" (Debug)");
+#endif
+                        writer.WriteLine();
+
+                        // Date and time
+                        writer.WriteLine("-> Date: " + DateTime.Now.ToString());
+
+                        // Unique report ID
+                        writer.WriteLine("-> Report ID: " + Guid.NewGuid().ToString());
+
+                        writer.WriteLine(SmallDashes);
+
+                        writer.WriteLine("-> OS Version: {0} ({1})", Environment.OSVersion, Microsoft.Devices.Environment.DeviceType);
+                        writer.WriteLine("-> Framework: " + Environment.Version.ToString());
+                        writer.WriteLine("-> Culture: " + CultureInfo.CurrentCulture);
+
+                        foreach (var infoCallback in AdditionalLogInfoCallbacks)
+                        {
+                            try
+                            {
+                                writer.WriteLine(SmallDashes);
+                                infoCallback(writer);
+                            }
+                            catch { }
+                        }
+
+                        writer.WriteLine(SmallDashes);
+
+                        // Message Body
+                        writer.WriteLine(message);
+
+                        writer.WriteLine();
+                    }
+                }
+            }
+            catch { }
+
+            if (sendImmediately)
+                SendExceptionLog();
+        }
+
+        #endregion
+
+        #region Log Info Hooks
+
+        private static List<Action<TextWriter>> _additionalLogInfoCallbacks = new List<Action<TextWriter>>();
+        public static List<Action<TextWriter>> AdditionalLogInfoCallbacks { get { return _additionalLogInfoCallbacks; } }
+
+        #endregion
+
+        #region HTTP Request and Response
+
+        private static string _errorLogContent;
+
+        private static void SendExceptionLog()
+        {
+            try
+            {
+                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    if (!store.FileExists(ErrorLogFilename))
+                        return;
+
+                    using (TextReader reader = new StreamReader(store.OpenFile(ErrorLogFilename, FileMode.Open, FileAccess.Read, FileShare.None)))
+                    {
+                        _errorLogContent = reader.ReadToEnd();
+                    }
+
+                    if (_errorLogContent == null)
+                        return;
+
+                    string url = ErrorReportURL + "?p=" + Utility.ApplicationName + "&v=" + Utility.ApplicationVersion;
+#if DEBUG
+                    url += "&d=1";
+#endif
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    Utility.PrepareHttpWebRequest(request);
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    request.Method = "POST";
+
+                    request.BeginGetRequestStream(new AsyncCallback(GetRequestStringCallback), request);
+                }
+            }
+            catch { }
+        }
+
+        private static void GetRequestStringCallback(IAsyncResult asyncResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asyncResult.AsyncState;
+
+            string postData = "log=" + HttpUtility.UrlEncode(_errorLogContent);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(postData);
+
+            try
+            {
+                Stream postStream = request.EndGetRequestStream(asyncResult);
+                postStream.Write(bytes, 0, bytes.Length);
+                postStream.Close();
+
+                request.BeginGetResponse(new AsyncCallback(GetResponseCallback), request);
+            }
+            catch { }
+        }
+
+        private static void GetResponseCallback(IAsyncResult asyncResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asyncResult.AsyncState;
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asyncResult);
+                Stream streamResponse = response.GetResponseStream();
+                StreamReader reader = new StreamReader(streamResponse);
+                string responseString = reader.ReadToEnd();
+
+                streamResponse.Close();
+                reader.Close();
+                response.Close();
+
+                // Delete the log file
+                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    store.DeleteFile(ErrorLogFilename);
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+    }
+}
