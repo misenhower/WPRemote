a9d68028cdd056605183bb065e82c0a754b4c01d
Matt Isenhower <matt@isenhower.com>
2012-03-07 15:34:11 -0800

    Added some properties to NetService.

--- CommonLibraries/Libraries/Komodex.Bonjour/Bonjour/NetService.cs
+++ CommonLibraries/Libraries/Komodex.Bonjour/Bonjour/NetService.cs
@@ -8,12 +8,58 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Komodex.Bonjour.DNS;
+using System.Collections.Generic;
 
 namespace Komodex.Bonjour
 {
     public class NetService
     {
+        #region Fields
+
+        private List<IPAddress> _ipAddresses = new List<IPAddress>();
+
+        #endregion
+
         #region Properties
+
+        /// <summary>
+        /// The name of the service, e.g., "17B30BC453C4B6A0"
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The service type, e.g., "_touch-able._tcp."
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// The service domain, e.g., "local."
+        /// </summary>
+        public string Domain { get; set; }
+
+        /// <summary>
+        /// The resolved hostname, e.g., "ike-mbp.local."
+        /// </summary>
+        public string Hostname { get; set; }
+
+        /// <summary>
+        /// All IP addresses for this service.
+        /// </summary>
+        public List<IPAddress> Addresses { get { return _ipAddresses; } }
+
+        /// <summary>
+        /// The port this service is listening on.
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// TXT Record data for this service.
+        /// </summary>
+        public Dictionary<string, string> TXTRecordData { get; set; }
+
+        internal DateTime TTLExpires { get; set; }
+
         #endregion
 
         #region Methods
