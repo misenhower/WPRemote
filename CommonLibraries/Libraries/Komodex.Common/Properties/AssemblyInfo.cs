c298ee36aeeea5893f2e2379f0dc77c6793adc8c
Matt Isenhower <matt@isenhower.com>
2012-10-24 16:33:46 -0700

    Added Komodex.Common project for Win8.

--- CommonLibraries/Libraries/Komodex.Common/Properties/AssemblyInfo.cs
+++ CommonLibraries/Libraries/Komodex.Common/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Komodex.Common.Phone")]
+[assembly: AssemblyTitle("Komodex.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Komodex Systems LLC")]
