1b173cf5c1d7a89692c1c6853a64c52e8f77c4fb
Matt Isenhower <matt@isenhower.com>
2012-11-11 20:57:59 -0800

    Added CrashReporter to Win8 CommonLibraries.

--- CommonLibraries/Libraries/Komodex.Common (WP7)/Analytics/CrashReporter.cs
+++ CommonLibraries/Libraries/Komodex.Common (WP7)/Analytics/CrashReporter.cs
@@ -1,10 +1,8 @@
 ﻿using Komodex.Common;
-using Microsoft.Phone.Info;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.IO.IsolatedStorage;
 using System.Net;
 using System.Text;
 using System.Windows;
@@ -28,13 +26,21 @@ namespace Komodex.Analytics
         /// PhoneAppCrashReporter.Initialize(application, rootFrame) instead.
         /// </summary>
         /// <param name="application"></param>
+#if WINDOWS_PHONE
         public static void Initialize(Application application)
+#else
+        public static void Initialize(Windows.UI.Xaml.Application application)
+#endif
         {
             if (_initialized)
                 return;
 
             // Hook into application unhandled exceptions
-            application.UnhandledException += App_UnhandledException;
+#if WINDOWS_PHONE
+            application.UnhandledException += (sender, e) => { LogException(e.ExceptionObject, "Unhandled Exception"); };
+#else
+            application.UnhandledException += (sender, e) => { LogMessage(e.Message, "Unhandled Exception"); };
+#endif
 
             // Send previous log if it exists
             SendExceptionLog();
@@ -42,11 +48,19 @@ namespace Komodex.Analytics
             _initialized = true;
         }
 
-        #region Events
+        #region Properties
 
-        private static void App_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        private static string ErrorReportPostURL
         {
-            LogException(e.ExceptionObject, "Unhandled Exception");
+            get
+            {
+
+                string url = ErrorReportURL + "?p=" + Utility.ApplicationName + "&v=" + Utility.ApplicationVersion;
+#if DEBUG
+                url += "&d=1";
+#endif
+                return url;
+            }
         }
 
         #endregion
@@ -66,80 +80,103 @@ namespace Komodex.Analytics
         {
             try
             {
-                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+                StringBuilder stringBuilder = new StringBuilder();
+                using (StringWriter writer = new StringWriter(stringBuilder))
                 {
-                    using (TextWriter writer = new StreamWriter(store.OpenFile(ErrorLogFilename, FileMode.Append)))
-                    {
-                        writer.WriteLine(LargeDashes);
+                    writer.WriteLine(LargeDashes);
 
-                        // Error type
-                        writer.WriteLine(type ?? "Application Error");
+                    // Error type
+                    writer.WriteLine(type ?? "Application Error");
 
-                        // Application name
-                        writer.WriteLine("-> Product: " + Utility.ApplicationName);
+                    // Application name
+                    writer.WriteLine("-> Product: " + Utility.ApplicationName);
 
-                        // Application version
-                        writer.Write("-> Version: " + Utility.ApplicationVersion);
+                    // Application version
+                    writer.Write("-> Version: " + Utility.ApplicationVersion);
 #if DEBUG
-                        writer.Write(" (Debug)");
+                    writer.Write(" (Debug)");
 #endif
-                        writer.WriteLine();
+                    writer.WriteLine();
 
-                        writer.WriteLine("-> Install date: " + FirstRunNotifier.FirstRunDate);
+                    writer.WriteLine("-> Install date: " + FirstRunNotifier.FirstRunDate);
 
-                        // Date and time
-                        writer.WriteLine("-> Date: " + DateTime.Now.ToString());
+                    // Date and time
+                    writer.WriteLine("-> Date: " + DateTime.Now.ToString());
 
-                        // Unique report ID
-                        writer.WriteLine("-> Report ID: " + Guid.NewGuid().ToString());
+                    // Unique report ID
+                    writer.WriteLine("-> Report ID: " + Guid.NewGuid().ToString());
 
-                        writer.WriteLine(SmallDashes);
+                    writer.WriteLine(SmallDashes);
 
-                        writer.WriteLine("-> OS Version: {0} ({1})", Environment.OSVersion, Microsoft.Devices.Environment.DeviceType);
-                        writer.WriteLine("-> Framework: " + Environment.Version.ToString());
-                        writer.WriteLine("-> Culture: " + CultureInfo.CurrentCulture);
+#if WINDOWS_PHONE
+                    writer.WriteLine("-> OS Version: {0} ({1})", Environment.OSVersion, Microsoft.Devices.Environment.DeviceType);
+                    writer.WriteLine("-> Framework: " + Environment.Version.ToString());
+#endif
+                    writer.WriteLine("-> Culture: " + CultureInfo.CurrentCulture);
 
-                        writer.WriteLine(SmallDashes);
+#if WINDOWS_PHONE
+                    writer.WriteLine(SmallDashes);
 
-                        writer.WriteLine("-> Device Manufacturer: " + DeviceStatus.DeviceManufacturer);
-                        writer.WriteLine("-> Device Name: " + DeviceStatus.DeviceName);
-                        writer.WriteLine("-> Device Hardware Version: " + DeviceStatus.DeviceHardwareVersion);
-                        writer.WriteLine("-> Device Firmware Version: " + DeviceStatus.DeviceFirmwareVersion);
+                    writer.WriteLine("-> Device Manufacturer: " + Microsoft.Phone.Info.DeviceStatus.DeviceManufacturer);
+                    writer.WriteLine("-> Device Name: " + Microsoft.Phone.Info.DeviceStatus.DeviceName);
+                    writer.WriteLine("-> Device Hardware Version: " + Microsoft.Phone.Info.DeviceStatus.DeviceHardwareVersion);
+                    writer.WriteLine("-> Device Firmware Version: " + Microsoft.Phone.Info.DeviceStatus.DeviceFirmwareVersion);
 
-                        writer.WriteLine(SmallDashes);
+                    writer.WriteLine(SmallDashes);
 
-                        // Memory usage
-                        var used = Utility.ReadableFilesize(DeviceStatus.ApplicationCurrentMemoryUsage);
-                        var peak = Utility.ReadableFilesize(DeviceStatus.ApplicationPeakMemoryUsage);
-                        writer.WriteLine("-> Memory Usage: {0} ({1} peak)", used, peak);
-                        var available = Utility.ReadableFilesize(DeviceStatus.ApplicationMemoryUsageLimit);
-                        var total = Utility.ReadableFilesize(DeviceStatus.DeviceTotalMemory);
-                        writer.WriteLine("-> Memory Availability: {0} of {1}", available, total);
+                    // Memory usage
+                    var used = Utility.ReadableFilesize(Microsoft.Phone.Info.DeviceStatus.ApplicationCurrentMemoryUsage);
+                    var peak = Utility.ReadableFilesize(Microsoft.Phone.Info.DeviceStatus.ApplicationPeakMemoryUsage);
+                    writer.WriteLine("-> Memory Usage: {0} ({1} peak)", used, peak);
+                    var available = Utility.ReadableFilesize(Microsoft.Phone.Info.DeviceStatus.ApplicationMemoryUsageLimit);
+                    var total = Utility.ReadableFilesize(Microsoft.Phone.Info.DeviceStatus.DeviceTotalMemory);
+                    writer.WriteLine("-> Memory Availability: {0} of {1}", available, total);
+#endif
 
-                        foreach (var infoCallback in AdditionalLogInfoCallbacks)
+                    foreach (var infoCallback in AdditionalLogInfoCallbacks)
+                    {
+                        try
                         {
-                            try
-                            {
-                                writer.WriteLine(SmallDashes);
-                                infoCallback(writer);
-                            }
-                            catch { }
+                            writer.WriteLine(SmallDashes);
+                            infoCallback(writer);
                         }
-
-                        writer.WriteLine(SmallDashes);
-
-                        // Message Body
-                        writer.WriteLine(message);
-
-                        writer.WriteLine();
+                        catch { }
                     }
+
+                    writer.WriteLine(SmallDashes);
+
+                    // Message Body
+                    writer.WriteLine(message);
+
+                    writer.WriteLine();
                 }
+
+                WriteLog(stringBuilder.ToString());
             }
+
+
             catch { }
 
             if (sendImmediately)
                 SendExceptionLog();
         }
+
+#if WINDOWS_PHONE
+        private static void WriteLog(string value)
+        {
+            using (var store = System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication())
+            using (TextWriter writer = new StreamWriter(store.OpenFile(ErrorLogFilename, FileMode.Append)))
+            {
+                writer.WriteLine(value);
+            }
+        }
+#else
+        private static void WriteLog(string value)
+        {
+            Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync(ErrorLogFilename, Windows.Storage.CreationCollisionOption.OpenIfExists).AsTask()
+                .ContinueWith((fileTask) => Windows.Storage.FileIO.AppendTextAsync(fileTask.Result, value).AsTask().Wait()).Wait();
+        }
+#endif
 
         #endregion
 
@@ -152,13 +189,14 @@ namespace Komodex.Analytics
 
         #region HTTP Request and Response
 
+#if WINDOWS_PHONE
         private static string _errorLogContent;
 
         private static void SendExceptionLog()
         {
             try
             {
-                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+                using (var store = System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication())
                 {
                     if (!store.FileExists(ErrorLogFilename))
                         return;
@@ -167,22 +205,17 @@ namespace Komodex.Analytics
                     {
                         _errorLogContent = reader.ReadToEnd();
                     }
-
-                    if (_errorLogContent == null)
-                        return;
-
-                    string url = ErrorReportURL + "?p=" + Utility.ApplicationName + "&v=" + Utility.ApplicationVersion;
-#if DEBUG
-                    url += "&d=1";
-#endif
-
-                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                    Utility.PrepareHttpWebRequest(request);
-                    request.ContentType = "application/x-www-form-urlencoded";
-                    request.Method = "POST";
-
-                    request.BeginGetRequestStream(new AsyncCallback(GetRequestStringCallback), request);
                 }
+
+                if (_errorLogContent == null)
+                    return;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ErrorReportPostURL);
+                Utility.PrepareHttpWebRequest(request);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Method = "POST";
+
+                request.BeginGetRequestStream(new AsyncCallback(GetRequestStringCallback), request);
             }
             catch { }
         }
@@ -222,13 +255,43 @@ namespace Komodex.Analytics
                 response.Close();
 
                 // Delete the log file
-                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+                using (var store = System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication())
                 {
                     store.DeleteFile(ErrorLogFilename);
                 }
             }
             catch { }
         }
+#else
+        private static async void SendExceptionLog()
+        {
+            try
+            {
+                var appFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                var file = await appFolder.GetFileAsync(ErrorLogFilename);
+                string errorLogContent = await Windows.Storage.FileIO.ReadTextAsync(file);
+
+                if (errorLogContent == null)
+                    return;
+
+                List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
+                postData.Add(new KeyValuePair<string, string>("log", errorLogContent));
+
+
+                using (var httpClient = new System.Net.Http.HttpClient())
+                using (var httpContent = new System.Net.Http.FormUrlEncodedContent(postData))
+                using (var response = await httpClient.PostAsync(ErrorReportPostURL, httpContent))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        await file.DeleteAsync();
+                    }
+                }
+            }
+            catch { }
+
+        }
+#endif
 
         #endregion
     }
