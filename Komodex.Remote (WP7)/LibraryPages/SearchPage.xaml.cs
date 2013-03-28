fc8b71b4fce8326b1247e45423c417e2c5a7fcca
Matt Isenhower <matt@isenhower.com>
2013-03-28 16:19:49 -0700

    Split the styles from App.xaml up into different theme files. This will allow different themes for WP8 where necessary.

--- Komodex.Remote (WP7)/LibraryPages/SearchPage.xaml.cs
+++ Komodex.Remote (WP7)/LibraryPages/SearchPage.xaml.cs
@@ -28,6 +28,10 @@ namespace Komodex.Remote.LibraryPages
 
             searchTimer.Interval = TimeSpan.FromMilliseconds(500);
             searchTimer.Tick += new EventHandler(searchTimer_Tick);
+
+#if WP7
+            lbSearchResults.GroupItemTemplate = (DataTemplate)App.Current.Resources["LongGroupItemTemplate"];
+#endif
         }
 
         private void DACPServerBoundPhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
