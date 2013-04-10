f944df7e7767f86a79a64dc6e8f49753f2e9b430
Matt Isenhower <matt@isenhower.com>
2013-04-10 13:58:53 -0700

    Recreated XAML sample data. Now using separate sample data classes for more reliable sample data generation. Sample data can be generated (and regenerated) in Blend from these classes.

---
+++ Komodex.Remote (WP7)/SampleData/SampleDataClasses.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if DEBUG
+namespace Komodex.Remote.SampleData
+{
+    class SampleDataDACPServer
+    {
+        public string LibraryName { get; set; }
+        public string CurrentArtist { get; set; }
+        public string CurrentAlbum { get; set; }
+        public string CurrentSongName { get; set; }
+        public int Volume { get; set; }
+        public List<SampleDataAirPlaySpeaker> Speakers { get; set; }
+
+        public List<SampleDataNamedItemBase> LibraryPlaylists { get; set; }
+        public List<SampleDataGeniusPlaylist> LibraryGeniusMixes { get; set; }
+        public List<SampleDataNamedItemGroup> LibraryArtists { get; set; }
+        public List<SampleDataNamedItemGroup> LibraryAlbums { get; set; }
+        public List<SampleDataNamedItemGroup> LibraryGenres { get; set; }
+        public List<SampleDataNamedItemGroup> LibraryPodcasts { get; set; }
+        public List<SampleDataNamedItemGroup> LibraryMovies { get; set; }
+        public List<SampleDataNamedItemGroup> LibraryTVShows { get; set; }
+    }
+
+    class SampleDataNamedItemBase
+    {
+        public string Name { get; set; }
+        public string SecondLine { get; set; }
+    }
+
+    #region Groups
+
+    class SampleDataGroupItems<T> : List<T>
+    {
+        public string Key { get; set; }
+    }
+
+    class SampleDataNamedItemGroup : SampleDataGroupItems<SampleDataNamedItemBase> { }
+
+    #endregion
+
+    #region Library Items
+
+    class SampleDataPlaylist : SampleDataNamedItemBase
+    {
+        public List<SampleDataPlaylistSong> Songs { get; set; }
+    }
+
+    class SampleDataPlaylistSong : SampleDataNamedItemBase
+    {
+        public string ArtistAndAlbum { get; set; }
+    }
+
+    class SampleDataGeniusPlaylist : SampleDataNamedItemBase
+    {
+        public string GeniusMixDescription { get; set; }
+    }
+
+    class SampleDataArtist : SampleDataNamedItemBase
+    {
+        public List<SampleDataNamedItemBase> Albums { get; set; }
+        public List<SampleDataArtistSong> Songs { get; set; }
+    }
+
+    class SampleDataArtistSong : SampleDataNamedItemBase
+    {
+        public string AlbumName { get; set; }
+    }
+
+    class SampleDataAlbum : SampleDataNamedItemBase
+    {
+        public string ArtistName { get; set; }
+        public List<SampleDataNamedItemBase> Songs { get; set; }
+    }
+
+    class SampleDataGenre : SampleDataNamedItemBase
+    {
+        public List<SampleDataNamedItemGroup> Artists { get; set; }
+        public List<SampleDataNamedItemGroup> Albums { get; set; }
+        public List<SampleDataNamedItemGroup> Songs { get; set; }
+    }
+
+    class SampleDataPodcast : SampleDataNamedItemBase
+    {
+        public List<SampleDataNamedItemBase> Episodes { get; set; }
+    }
+
+    #endregion
+
+    #region AirPlay Speakers
+
+    class SampleDataAirPlaySpeaker
+    {
+        public string Name { get; set; }
+        public bool BindableActive { get; set; }
+        public bool HasVideo { get; set; }
+        public bool HasPassword { get; set; }
+        public int BindableVolume { get; set; }
+    }
+
+    #endregion
+}
+#endif
