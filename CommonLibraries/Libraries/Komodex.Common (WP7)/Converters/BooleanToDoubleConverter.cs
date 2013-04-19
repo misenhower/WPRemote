d9e7b40990cf4ac66b5b4a52c5e98b6258a9fdf2
Matt Isenhower <matt@isenhower.com>
2013-04-18 17:11:40 -0700

    Added BooleanToDoubleConverter (for binding Opacity values, etc.)

---
+++ CommonLibraries/Libraries/Komodex.Common (WP7)/Converters/BooleanToDoubleConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Komodex.Common.Converters
+{
+    public class BooleanToDoubleConverter : BooleanConverterBase<double>
+    {
+    }
+}
