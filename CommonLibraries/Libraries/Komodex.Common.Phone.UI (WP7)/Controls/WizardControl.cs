c03cc435dc1f411d2694f733cc8b5a807fdb76e9
Matt Isenhower <matt@isenhower.com>
2013-05-05 21:49:02 -0700

    Added item transition functionality to WizardControl.

--- CommonLibraries/Libraries/Komodex.Common.Phone.UI (WP7)/Controls/WizardControl.cs
+++ CommonLibraries/Libraries/Komodex.Common.Phone.UI (WP7)/Controls/WizardControl.cs
@@ -8,11 +8,15 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace Komodex.Common.Phone.Controls
 {
     public class WizardControl : ItemsControl
     {
+        private Storyboard _currentStoryboard;
+        private IEasingFunction _animationEasingFunction = new QuadraticEase() { EasingMode = EasingMode.EaseInOut };
+
         public WizardControl()
         {
             DefaultStyleKey = typeof(WizardControl);
@@ -33,14 +37,7 @@ namespace Komodex.Common.Phone.Controls
         public int SelectedIndex
         {
             get { return _selectedIndex; }
-            set
-            {
-                if (_selectedIndex == value)
-                    return;
-
-                _selectedIndex = value;
-                // TODO: Update display
-            }
+            set { SetSelectedIndex(value); }
         }
 
         #endregion
@@ -108,6 +105,8 @@ namespace Komodex.Common.Phone.Controls
 
         #endregion
 
+        #region Overrides
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
@@ -117,6 +116,13 @@ namespace Komodex.Common.Phone.Controls
         private void WizardControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             UpdateDesignModeDisplay();
+
+            if (!DesignerProperties.IsInDesignTool)
+            {
+                var geometry = new RectangleGeometry();
+                geometry.Rect = new Rect(0, 0, ActualWidth, ActualHeight);
+                Clip = geometry;
+            }
         }
 
         protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
@@ -131,7 +137,150 @@ namespace Komodex.Common.Phone.Controls
 
             WizardItem wizardItem = (WizardItem)element;
             wizardItem.RenderTransform = new TranslateTransform();
+
+            if (!DesignerProperties.IsInDesignTool)
+            {
+                if (Items.IndexOf(wizardItem) != _selectedIndex)
+                    wizardItem.Visibility = System.Windows.Visibility.Collapsed;
+            }
         }
+
+        #endregion
+
+        #region Step Animation
+
+        public void SetSelectedIndex(int value, bool useTransitions = true)
+        {
+            if (_selectedIndex == value)
+                return;
+
+            SetSelectedIndex(_selectedIndex, value, useTransitions);
+        }
+
+        private void SetSelectedIndex(int oldValue, int value, bool useTransitions = true)
+        {
+            if (oldValue == value)
+                return;
+
+            int oldIndex = oldValue;
+            _selectedIndex = value;
+
+            if (oldIndex < 0 || value < 0)
+                useTransitions = false;
+            if (oldIndex >= Items.Count || value >= Items.Count)
+                useTransitions = false;
+
+            // If we're in design mode, just update the item positions and return
+            if (DesignerProperties.IsInDesignTool)
+            {
+                UpdateDesignModeDisplay();
+                return;
+            }
+
+            // If we aren't using transitions, cancel any previous animations and update the display
+            if (!useTransitions)
+            {
+                if (_currentStoryboard != null)
+                {
+                    _currentStoryboard.Stop();
+                    _currentStoryboard = null;
+                }
+
+                for (int i = 0; i < Items.Count; i++)
+                {
+                    WizardItem item = (WizardItem)Items[i];
+                    if (i == _selectedIndex)
+                    {
+                        item.Visibility = System.Windows.Visibility.Visible;
+                        ((TranslateTransform)item.RenderTransform).X = 0;
+                    }
+                    else
+                    {
+                        item.Visibility = System.Windows.Visibility.Collapsed;
+                    }
+                }
+
+                return;
+            }
+
+            // Prepare the new item (off screen)
+            // Doing this early prevents lag/jumpiness in the middle of the animation.
+            WizardItem newItem = (WizardItem)Items[_selectedIndex];
+            ((TranslateTransform)newItem.RenderTransform).X = ActualWidth;
+            newItem.Visibility = System.Windows.Visibility.Visible;
+
+            // If we're already animating, don't do anything since the new index will be handled automatically
+            if (_currentStoryboard != null)
+                return;
+
+            // Set up transitions
+            double sourceX, destX;
+            if (oldIndex < SelectedIndex)
+            {
+                sourceX = ActualWidth;
+                destX = -ActualWidth;
+            }
+            else
+            {
+                sourceX = -ActualWidth;
+                destX = ActualWidth;
+            }
+
+            // Divide the animation time by two since it will be used twice (once for the outgoing item, and again for the incoming item)
+            TimeSpan halfAnimationTime = new TimeSpan(AnimationDuration.Ticks / 2);
+            Duration duration = new Duration(halfAnimationTime);
+            duration = new Duration(AnimationDuration);
+
+            if (_currentStoryboard != null)
+                _currentStoryboard.Stop();
+            _currentStoryboard = new Storyboard();
+            _currentStoryboard.Duration = duration;
+
+            // Outgoing item animation
+            WizardItem oldItem = (WizardItem)Items[oldIndex];
+            DoubleAnimation itemAnimation = new DoubleAnimation();
+            itemAnimation.Duration = duration;
+            itemAnimation.EasingFunction = _animationEasingFunction;
+            Storyboard.SetTarget(itemAnimation, oldItem.RenderTransform);
+            Storyboard.SetTargetProperty(itemAnimation, new PropertyPath(TranslateTransform.XProperty));
+            _currentStoryboard.Children.Add(itemAnimation);
+            itemAnimation.To = destX;
+            _currentStoryboard.Completed += (s1, e1) =>
+            {
+                oldItem.Visibility = System.Windows.Visibility.Collapsed;
+
+                // Get the new item
+                int newIndex = _selectedIndex;
+                newItem = (WizardItem)Items[newIndex];
+
+                // Set up the new item (off screen)
+                ((TranslateTransform)newItem.RenderTransform).X = sourceX;
+                newItem.Visibility = System.Windows.Visibility.Visible;
+
+                // Create a new storyboard
+                _currentStoryboard = new Storyboard();
+                _currentStoryboard.Duration = duration;
+
+                // Incoming item animation
+                itemAnimation = new DoubleAnimation();
+                itemAnimation.Duration = duration;
+                itemAnimation.EasingFunction = _animationEasingFunction;
+                Storyboard.SetTarget(itemAnimation, newItem.RenderTransform);
+                Storyboard.SetTargetProperty(itemAnimation, new PropertyPath(TranslateTransform.XProperty));
+                _currentStoryboard.Children.Add(itemAnimation);
+                itemAnimation.To = 0;
+                _currentStoryboard.Completed += (s2, e2) =>
+                {
+                    _currentStoryboard = null;
+                    SetSelectedIndex(newIndex, _selectedIndex);
+                };
+
+                _currentStoryboard.Begin();
+            };
+            _currentStoryboard.Begin();
+        }
+
+        #endregion
 
     }
 }
--- CommonLibraries/TestApps/WP7/Komodex.CommonLibrariesTestApp/WizardControl/WizardControlTestPage.xaml.cs
+++ CommonLibraries/TestApps/WP7/Komodex.CommonLibrariesTestApp/WizardControl/WizardControlTestPage.xaml.cs
@@ -16,5 +16,37 @@ namespace Komodex.CommonLibrariesTestApp.WizardControl
         {
             InitializeComponent();
         }
+
+        private void AnimatedNavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateTo((string)((Button)sender).Tag, true);
+        }
+
+        private void InstantNavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateTo((string)((Button)sender).Tag, false);
+        }
+
+        private void NavigateTo(string page, bool animated)
+        {
+            switch (page)
+            {
+                case "1":
+                    wizard.SetSelectedIndex(0, animated);
+                    break;
+
+                case "2":
+                    wizard.SetSelectedIndex(1, animated);
+                    break;
+
+                case "3":
+                    wizard.SetSelectedIndex(2, animated);
+                    break;
+
+                case "4":
+                    wizard.SetSelectedIndex(3, animated);
+                    break;
+            }
+        }
     }
 }
