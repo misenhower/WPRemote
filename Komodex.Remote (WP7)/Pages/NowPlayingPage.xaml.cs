e4a982084b91cc61747e546a4a0d58ecfa55377b
Matt Isenhower <matt@isenhower.com>
2013-06-30 14:50:15 -0700

    Added shuffle and repeat buttons to the new Now Playing page.

--- Komodex.Remote (WP7)/Pages/NowPlayingPage.xaml.cs
+++ Komodex.Remote (WP7)/Pages/NowPlayingPage.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Komodex.Common;
+using Komodex.DACP;
 
 namespace Komodex.Remote.Pages
 {
@@ -22,5 +25,65 @@ namespace Komodex.Remote.Pages
             ApplicationBar.Mode = ApplicationBarMode.Minimized;
             ApplicationBar.BackgroundColor = (Color)Application.Current.Resources["PhoneBackgroundColor"];
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            UpdatePlayModeButtons();
+        }
+
+        protected override void CurrentServer_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.CurrentServer_PropertyChanged(sender, e);
+
+            switch (e.PropertyName)
+            {
+                case "ShuffleState":
+                case "RepeatState":
+                    UpdatePlayModeButtons();
+                    break;
+            }
+        }
+
+        #region Play Mode Buttons
+
+        protected const double _playModeButtonOpacityOff = 0.4;
+        protected const double _playModeButtonOpacityOn = 0.8;
+
+        protected readonly ImageSource _repeatIcon = new BitmapImage(new Uri("/Assets/Icons/Repeat.png", UriKind.Relative));
+        protected readonly ImageSource _repeatOneIcon = new BitmapImage(new Uri("/Assets/Icons/RepeatOne.png", UriKind.Relative));
+
+        protected void UpdatePlayModeButtons()
+        {
+            if (CurrentServer == null || !CurrentServer.IsConnected)
+                return;
+
+            // Shuffle button
+            ShuffleImage.Opacity = (CurrentServer.ShuffleState) ? _playModeButtonOpacityOn : _playModeButtonOpacityOff;
+
+            // Repeat button
+            RepeatImage.Opacity = (CurrentServer.RepeatState != RepeatStates.None) ? _playModeButtonOpacityOn : _playModeButtonOpacityOff;
+            RepeatImage.Source = (CurrentServer.RepeatState != RepeatStates.RepeatOne) ? _repeatIcon : _repeatOneIcon;
+        }
+
+        private void ShuffleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentServer == null || !CurrentServer.IsConnected)
+                return;
+
+            CurrentServer.SendShuffleStateCommand();
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentServer == null || !CurrentServer.IsConnected)
+                return;
+
+            CurrentServer.SendRepeatStateCommand();
+        }
+
+        #endregion
+
     }
 }
