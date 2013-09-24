dd7b2a419fd95fd63262b5d43b660f67393f8e70
Matt Isenhower <matt@isenhower.com>
2013-09-23 17:35:49 -0700

    Added Wake-on-LAN.

---
+++ CommonLibraries/Libraries/Komodex.Common (WP7)/Networking/WakeOnLAN.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+#if !WP7
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+#endif
+
+namespace Komodex.Networking
+{
+    public static class WakeOnLAN
+    {
+        private static readonly IPAddress BroadcastAddress = IPAddress.Broadcast;
+        private const int BroadcastPort = 9;
+
+        public static Task<bool> SendWOLPacket(byte[] macAddress)
+        {
+            if (macAddress.Length != 6)
+                throw new ArgumentException("macAddress");
+
+            // Generate WOL message
+            // Message is 6 0xFF bytes followed by the MAC address repeated 16 times.
+            byte[] message = new byte[6 + 16 * 6];
+            for (int i = 0; i < 6; i++)
+                message[i] = 0xFF;
+            for (int i = 6; i < message.Length; i++)
+                message[i] = macAddress[i % 6];
+
+            // Send the message
+            return SendMessage(message);
+        }
+
+#if WP7
+        private static Task<bool> SendMessage(byte[] message)
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            SocketAsyncEventArgs socketArgs = new SocketAsyncEventArgs();
+            socketArgs.RemoteEndPoint = new IPEndPoint(BroadcastAddress, BroadcastPort);
+            socketArgs.SetBuffer(message, 0, message.Length);
+            socketArgs.Completed += (sender, e) =>
+            {
+                bool success = (e.SocketError == SocketError.Success);
+                try { socket.Shutdown(SocketShutdown.Both); }
+                catch { success = false; }
+                try { socket.Close(); }
+                catch { success = false; }
+                tcs.TrySetResult(success);
+            };
+
+            try
+            {
+                socket.SendToAsync(socketArgs);
+            }
+            catch
+            {
+                tcs.TrySetResult(false);
+            }
+
+            return tcs.Task;
+        }
+#else
+        private static async Task<bool> SendMessage(byte[] message)
+        {
+            try
+            {
+                using (DatagramSocket socket = new DatagramSocket())
+                {
+                    var stream = await socket.GetOutputStreamAsync(new HostName(BroadcastAddress.ToString()), BroadcastPort.ToString());
+                    var writer = new DataWriter(stream);
+                    writer.WriteBytes(message);
+                    await writer.StoreAsync();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+#endif
+    }
+}
