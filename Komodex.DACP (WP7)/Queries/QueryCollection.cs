89f207f1d072d921d9633646ee1c96fc08004896
Matt Isenhower <matt@isenhower.com>
2013-10-05 12:53:38 -0700

    Added DACP Query classes.

---
+++ Komodex.DACP (WP7)/Queries/QueryCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Komodex.DACP.Queries
+{
+    internal class QueryCollection : QueryElement
+    {
+        private const string AndOperator = "+";
+        private const string OrOperator = ",";
+
+        private readonly string _operator;
+        private readonly List<QueryElement> _elements;
+
+        private QueryCollection(string op, params QueryElement[] args)
+        {
+            _operator = op;
+            _elements = new List<QueryElement>(args);
+        }
+
+        public static QueryCollection And(params QueryElement[] args)
+        {
+            return new QueryCollection(AndOperator, args);
+        }
+
+        public static QueryCollection Or(params QueryElement[] args)
+        {
+            return new QueryCollection(OrOperator, args);
+        }
+
+        public void Add(QueryElement element)
+        {
+            _elements.Add(element);
+        }
+
+        public override string ToString()
+        {
+            return "(" + string.Join(_operator, _elements) + ")";
+        }
+    }
+}
---
+++ Komodex.DACP (WP7)/Queries/QueryPredicate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Komodex.DACP.Queries
+{
+    internal class QueryPredicate : QueryElement
+    {
+        private const string IsOperator = ":";
+        private const string IsNotOperator = "!:";
+
+        private readonly string _key;
+        private readonly string _value;
+        private readonly string _operator;
+
+        private QueryPredicate(string key, string value, string op)
+        {
+            _key = key;
+            _value = value;
+            _operator = op;
+        }
+
+        public static QueryPredicate Is(string key, string value)
+        {
+            return new QueryPredicate(key, value, IsOperator);
+        }
+
+        public static QueryPredicate Is(string key, int value)
+        {
+            return Is(key, value.ToString());
+        }
+
+        public static QueryPredicate IsNot(string key, string value)
+        {
+            return new QueryPredicate(key, value, IsNotOperator);
+        }
+
+        public static QueryPredicate IsNot(string key, int value)
+        {
+            return IsNot(key, value.ToString());
+        }
+
+        public override string ToString()
+        {
+            return "'" + _key + _operator + _value + "'";
+        }
+    }
+}
