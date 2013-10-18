7dc032f583ed052d00dca915aca82b1e7cbb6797
Matt Isenhower <matt@isenhower.com>
2013-10-17 19:15:59 -0700

    Removing BrowseLibraryPage.

--- Komodex.Remote (WP7)/Navigation/NavigationManager.cs
+++ Komodex.Remote (WP7)/Navigation/NavigationManager.cs
@@ -137,21 +137,6 @@ namespace Komodex.Remote
 
         #endregion
 
-        public static void OpenBrowseLibraryPage(int databaseID, ContainerType containerType)
-        {
-            Navigate("/LibraryPages/BrowseLibraryPage.xaml?databaseID={0}&containerType={1}", databaseID, containerType);
-        }
-
-        public static void OpenBrowseLibraryPage(int databaseID, ContainerType containerType, int groupID)
-        {
-            Navigate("/LibraryPages/BrowseLibraryPage.xaml?databaseID={0}&containerType={1}&groupID={2}", databaseID, containerType, groupID);
-        }
-
-        public static void OpenBrowseLibraryPage(int databaseID, int playlistID)
-        {
-            Navigate("/LibraryPages/BrowseLibraryPage.xaml?databaseID={0}&containerType={1}&playlistID={2}", databaseID, ContainerType.Playlist, playlistID);
-        }
-
         public static void OpenGenrePage(string genreName)
         {
             genreName = Uri.EscapeDataString(genreName);
