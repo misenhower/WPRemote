2a284da115cda623b59e703ee03e6f3ee0860c8d
Matt Isenhower <matt@isenhower.com>
2013-10-22 12:43:15 -0700

    Added ItemCountConverter. Changed the number format for "items" display to include thousands separators. Updated PlaylistTemplate to show item counts.

---
+++ Komodex.Remote (WP7)/Converters/ItemCountConverter.cs
@@ -0,0 +1,30 @@
+﻿using Komodex.Remote.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Komodex.Remote.Converters
+{
+    public class ItemCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int)
+            {
+                int count = (int)value;
+                if (count == 1)
+                    return LocalizedStrings.OneItem;
+                return string.Format(LocalizedStrings.MultipleItems, count);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
