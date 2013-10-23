58604cc9de8f7e93d91803df1631af34082c6d08
Matt Isenhower <matt@isenhower.com>
2013-10-23 14:28:32 -0700

    Added icons for each type of playlist. Added PlaylistTypeToImageSourceConverter.

---
+++ Komodex.Remote (WP7)/Converters/PlaylistTypeToImageSourceConverter.cs
@@ -0,0 +1,33 @@
+﻿using Komodex.DACP.Containers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Komodex.Remote.Converters
+{
+    public class PlaylistTypeToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Playlist playlist = value as Playlist;
+            if (playlist != null)
+            {
+                if (playlist.IsSmartPlaylist)
+                    return new Uri("/Assets/Icons/Playlist.Smart.png", UriKind.Relative);
+                if (playlist.IsSavedGenius)
+                    return new Uri("/Assets/Icons/Playlist.SavedGenius.png", UriKind.Relative);
+                if (playlist.HasChildContainers)
+                    return new Uri("/Assets/Icons/Playlist.Folder.png", UriKind.Relative);
+            }
+
+            return new Uri("/Assets/Icons/Playlist.Normal.png", UriKind.Relative);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
