11be12b44875c1115a04d12185e6875a5ae00b9f
Matt Isenhower <matt@isenhower.com>
2013-10-27 17:42:52 -0700

    Added some converters to help display search results. Added some styles for search results.

---
+++ Komodex.Remote (WP7)/Converters/SearchResultSectionHeaderConverter.cs
@@ -0,0 +1,40 @@
+﻿using Komodex.DACP.Groups;
+using Komodex.DACP.Items;
+using Komodex.Remote.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Komodex.Remote.Converters
+{
+    public class SearchResultSectionHeaderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Type type = value as Type;
+            if (type != null)
+            {
+                // Albums
+                if (type == typeof(Album))
+                    return LocalizedStrings.LibraryAlbumsPivotItem;
+
+                // Artists
+                if (type == typeof(Artist))
+                    return LocalizedStrings.LibraryArtistsPivotItem;
+
+                // Songs
+                if (type == typeof(Song))
+                    return LocalizedStrings.LibrarySongsPivotItem;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Komodex.Remote (WP7)/Converters/SearchResultsSecondLineConverter.cs
@@ -0,0 +1,40 @@
+﻿using Komodex.DACP.Groups;
+using Komodex.DACP.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Komodex.Remote.Converters
+{
+    public class SearchResultsSecondLineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            Type type = value.GetType();
+
+            // Albums
+            if (type == typeof(Album))
+                return ((Album)value).ArtistName;
+
+            // Artists
+            if (type == typeof(Artist))
+                return null;
+
+            // Songs
+            if (type == typeof(Song))
+                return ((Song)value).ArtistAndAlbumName;
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
