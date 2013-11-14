e1fb4c018f2e24e8f37f293215e54bc2626aae1f
Matt Isenhower <matt@isenhower.com>
2013-11-14 13:44:19 -0800

    Added ObservableCollectionEx.

---
+++ CommonLibraries/Libraries/Komodex.Common (WP7)/ObservableCollectionEx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Komodex.Common
+{
+    public class ObservableCollectionEx<T> : ObservableCollection<T>
+    {
+        public ObservableCollectionEx()
+        { }
+
+        public ObservableCollectionEx(IEnumerable<T> collection)
+            : base(collection)
+        { }
+
+        public ObservableCollectionEx(List<T> list)
+            : base(list)
+        { }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+                Items.Add(item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public void ReplaceContents(IEnumerable<T> collection)
+        {
+            Items.Clear();
+            foreach (var item in collection)
+                Items.Add(item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
