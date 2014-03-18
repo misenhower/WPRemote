c9b46203c43b9401708530610ebb51184a644370
Matt Isenhower <matt@isenhower.com>
2014-03-17 20:42:50 -0700

    Added SendPropertyChanged to ObservableCollectionEx.

--- CommonLibraries/Libraries/Komodex.Common (WP7)/ObservableCollectionEx.cs
+++ CommonLibraries/Libraries/Komodex.Common (WP7)/ObservableCollectionEx.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Komodex.Common
@@ -33,6 +35,14 @@ namespace Komodex.Common
             foreach (var item in collection)
                 Items.Add(item);
             OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        protected void SendPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException("propertyName");
+
+            ThreadUtility.RunOnUIThread(() => OnPropertyChanged(new PropertyChangedEventArgs(propertyName)));
         }
     }
 }
